e1d098fc20a3d469597853c4723ded2e4fa8dd33
Karol Golec <karol.itgolo@gmail.com>
2017-11-29 17:06:10 +0100

    to home work

---
+++ src/main/java/pl/itgolo/libs/skeletongradle/GreetingPlugin.java
@@ -0,0 +1,17 @@
+package pl.itgolo.libs.skeletongradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * The type Greeting plugin.
+ */
+public class GreetingPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        project.task("hello").doLast((task) -> {
+            System.out.printf("Hello {}", "one");
+        });
+    }
+}
